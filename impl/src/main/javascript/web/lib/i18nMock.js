2e9fc6e64be5d917807c16fbc991cdda8c9b9dd6
Francisco Guilherme <fguilherme@pentaho.com>
2016-06-28 17:54:28 +0100

    Change i18nMock path for tests compatibility

--- impl/src/main/javascript/web/lib/i18nMock.js
+++ impl/src/main/javascript/web/lib/i18nMock.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2010 - 2016 Pentaho Corporation.  All rights reserved.
+ * Copyright 2010 - 2016 Pentaho Corporation. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-define(["pentaho/util/MessageBundle", "json"], function(MessageBundle) {
+
+/**
+ * The i18N DUMMY data service
+ *
+ * Use while i18n service is TBD
+ *
+ * @module i18nMock
+ * @property {Function} load Loads the i18n definitions
+ * @property {Function} normalize Normalizes the i18n ID
+ */
+define(["pentaho/util/MessageBundle", "text"], function(MessageBundle) {
   return {
     load: function(bundlePath, require, onLoad, config) {
       var bundleUrl = require.toUrl(bundlePath) + ".properties";
@@ -21,23 +31,23 @@ define(["pentaho/util/MessageBundle", "json"], function(MessageBundle) {
         onLoad(new MessageBundle(parseProperties(bundleText)));
       });
     },
-    normalize: function(name, normalize) {
+    normalize: function (name, normalize) {
       return normalize(getBundleID(name));
     }
   };
 
   function getBundleID(bundlePath) {
     var bundleMid;
-    if(!bundlePath) {
+    if (!bundlePath) {
       // "pentaho/i18n!"
       // Use the default location and bundle.
       bundleMid = "./i18n/messages";
-    } else if(bundlePath[0] === "/") {
+    } else if (bundlePath[0] === "/") {
       // "pentaho/i18n!/pentaho/common/nls/messages"
       // The path is, directly, an absolute module id of a message bundle (without the /).
       bundleMid = bundlePath.substr(1);
-      if(!bundleMid) throw new Error("[pentaho/i18n!] Bundle path argument cannot be a single '/'.");
-    } else if(bundlePath[0] !== "." && bundlePath.indexOf("/") < 0) {
+      if (!bundleMid) throw new Error("[pentaho/i18n!] Bundle path argument cannot be a single '/'.");
+    } else if (bundlePath[0] !== "." && bundlePath.indexOf("/") < 0) {
 
       // the name of a bundle in the default "./i18n" sub-module
       bundleMid = "./i18n/" + bundlePath;
