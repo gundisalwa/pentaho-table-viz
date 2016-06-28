16a44c805d5ec90d9eb776e0b9b0d319ebadb0bd
Francisco Guilherme <fguilherme@pentaho.com>
2016-06-28 17:59:26 +0100

    Setup tests dependencies

--- impl/src/test/config/javascript/context.js
+++ impl/src/test/config/javascript/context.js
@@ -9,14 +9,28 @@ var src = "src/main/javascript/web";
 requirejs.config({
   baseUrl: "/base",
   paths: {
-    "css": depWebJars + "/require-css/${dependency.require-css.version}/css",
-    "text": depWebJars + "/requirejs-text/${dependency.requirejs-text.version}/text",
+    "css": depWebJars + "/require-css/${require-css.version}/css",
+    "text": depWebJars + "/requirejs-text/${requirejs-text.version}/text",
     "pentaho": depDir + "/common-ui/resources/web/pentaho",
-    "pentaho/i18n": src + "/lib/i18nMock"
+    "pentaho/i18n": src + "/lib/i18nMock",
+    "viz-datatable": src + "/viz-datatable",
+
+    "underscorejs": depWebJars + "/underscorejs/${underscore.version}/underscore",
+    "jquery": depWebJars + "/jquery/${jquery.version}/jquery",
+    "bootstrap-css": depWebJars + "/bootstrap-css/${bootstrap.version}/css/bootstrap",
+
+    "datatables.net": depWebJars + "/datatables.net/${datatables.net.version}/js/jquery.dataTables",
+    "datatables.net-bs": depWebJars + "/datatables.net-bs/${datatables.net-bs.version}/js/dataTables.bootstrap",
+    "datatables.net-fixedheader": depWebJars + "/datatables.net-fixedheader/${datatables.net-fixedheader.version}/js/dataTables.fixedHeader",
+    "datatables.net-fixedheader-bs": depWebJars + "/datatables.net-fixedheader-bs/${datatables.net-fixedheader-bs.version}",
+    "datatables.net-scroller": depWebJars + "/datatables.net-scroller/${datatables.net-scroller.version}/js/dataTables.scroller",
+    "datatables.net-scroller-bs": depWebJars + "/datatables.net-scroller-bs/${datatables.net-scroller-bs.version}",
+    "datatables.net-colreorder": depWebJars + "/datatables.net-colreorder/${datatables.net-colreorder.version}/js/dataTables.colReorder",
+    "datatables.net-colreorder-bs": depWebJars + "/datatables.net-colreorder-bs/${datatables.net-colreorder-bs.version}"
   },
   map: {
     "*": {
-      i18n: "pentaho/i18n",
+      "i18n": "pentaho/i18n",
       "pentaho/type/theme": "pentaho/type/themes/crystal"
     }
   },
@@ -30,5 +44,3 @@ requirejs.config({
     window.__karma__.start();
   }
 });
-
-var CONTEXT_PATH = "/base/";
